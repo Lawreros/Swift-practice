7cd5a05d65d66493da65e1e843ef910f7d5680f0
Lawreros <lawreros@gmail.com>
2022-01-30 21:54:39 -0500

    Initial Commit

---
+++ Swift-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-practice
+//
+//  Created by Ross on 1/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-practice/Swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_practiceApp.swift
+//  Swift-practice
+//
+//  Created by Ross on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
