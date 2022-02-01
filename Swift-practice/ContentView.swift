27abd8fc2a26062db236605ee4486b00b7a7d62e
Lawreros <lawreros@gmail.com>
2022-02-01 16:45:22 -0500

    snapshot before using LazyVGrid

--- Swift-practice/ContentView.swift
+++ Swift-practice/ContentView.swift
@@ -8,21 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗","🛵","🚂","🚁","🚑","✈️","🚜"]
+    @State var emojiCount = 3 //number of rectangles that we want
+    
     var body: some View {
-        HStack{//Horizontal Stack
-            CardView()
-            CardView(isFaceUp: false) //overrides any default value for a variable inside the struct/view
-            CardView()
-            CardView()
+        VStack{//Verticle Stack (From the top down)
+            HStack{//Horizontal Stack
+    //            CardView(content: emojis[0])
+    //            CardView(isFaceUp: false,content: emojis[1]) //overrides any default value for a variable inside the struct/view
+    //            CardView(content: emojis[2])
+    //            CardView(content: emojis[3])
+                
+                ForEach(emojis[0...emojiCount], id: \.self){ emoji in // the id: \.self assigns each emoji an identifier that is itself. So the string "🚗" now has its id = "🚗". So in a dict sense {🚗:"🚗", 🚁:"🚁"}
+                    CardView(content: emoji)
+                }
+            }
+            .padding(.horizontal)
+            .foregroundColor(.red)
+            
+            HStack{
+                remove //button view defined below
+                Spacer() //takes up as much space as possible
+                add //button view defined below
+            }
+            .font(.largeTitle)//the font size extends to certain image types if supported by apple
+            .padding(.horizontal)
         }
-        .padding(.horizontal)
-        .foregroundColor(.red)
+        
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        }, label:{
+//            VStack {
+//                Text("Remove")
+//                Text("Card")
+//            }
+            Image(systemName: "minus.circle")
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojis.count-1 {
+                emojiCount += 1
+            }
+        }, label:{
+//            VStack {
+//                Text("Add")
+//                Text("Card")
+//            }
+            Image(systemName: "plus.circle")
+        })
     }
 }
 
 struct CardView: View{
-    var isFaceUp: Bool = true //all variables have to have an initial value or be provided one as an argument (get rid of "= true" and put "isFaceUp: true" in the arguments of the "CardView()" stuff above
+    @State var isFaceUp: Bool = true //all variables have to have an initial value or be provided one as an argument (get rid of "= true" and put "isFaceUp: true" in the arguments of the "CardView()" stuff above
     
+    //@State points to some external memory. Because Views/structs are immutable but also run through all of the functions they contain each time they are called, this allows for a somewhat hacky way to change the view state.
+    
+    var content: String
     
     var body: some View{// this is a var because it changes every time someone asks for it, as it excutes the contained function(s) to determine its value
         ZStack {
@@ -34,13 +83,15 @@ struct CardView: View{
             
                 shape.stroke(lineWidth: 3)//stroke view modifier now makes this view all about the border. If you were to put .fill(.white) at the end of this view, you would actually be filling the outline view made by stroke.
             
-                Text("✈️")
+                Text(content)
                     .font(.largeTitle)
             } else {
                 RoundedRectangle(cornerRadius: 20)
                     .fill()
                 
             }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp //isFaceUp is a value stored outside of the struct
         }
     }
 }
