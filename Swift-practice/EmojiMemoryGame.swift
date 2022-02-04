50dd8654a882eea327d5881b84d74bff6c4e7f1e
Lawreros <lawreros@gmail.com>
2022-02-04 15:47:46 -0500

    finish Lecture 3

---
+++ Swift-practice/EmojiMemoryGame.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiMemoryGame.swift
+//  Swift-practice
+//
+//  Created by Ross on 2/4/22.
+//
+
+import SwiftUI
+
+//func makeCardContent(index: Int) -> String {
+//    return "😀"
+//}
+
+
+class EmojiMemoryGame {
+    static let emojis = ["🚗","🛵","🚂","🚁","🚑","✈️","🚜"] //static means that emojis is seen globally as EmojiMemoryGame.emojis
+    
+    
+    private var model: MemoryGame<String> =
+    MemoryGame<String>(numberOfPairsOfCards: 4, createCardContent: {//(index: Int) -> String in //in seperates the arguments from the code
+        //return "😀"
+        pairIndex in emojis[pairIndex] //function simplified because the init of MemoryGame knows the function takes an Int and returns a String
+        
+    })
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
--- Swift-practice/MemoryGame.swift
+++ Swift-practice/MemoryGame.swift
@@ -7,15 +7,27 @@
 
 import Foundation //basic library for Swift that doesn't include a lot of UI stuff
 
-struct MemoryGame {
+struct MemoryGame<CardContent> { //defined by the user of this struct. CardContent is a "don't care" variable
     var cards: Array<Card>
     
     func choose(_: Card) {
         
     }
     
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        // add numberOfPairsOfCards x 2 cards to cards array
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content: CardContent = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     struct Card { //This struct is called MemoryGame.Card outside of this struct
-        
+        var isFaceUp: Bool = false
+        var isMatches: Bool = false
+        var content: CardContent //want to keep this flexible as to what symbol/number/image is on the card
     }
     
 }
