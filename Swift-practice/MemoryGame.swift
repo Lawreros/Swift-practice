2213a27b28ba270b011971a260683cb26754e453
Lawreros <lawreros@gmail.com>
2022-02-04 14:47:41 -0500

    make MemoryGame logic file

---
+++ Swift-practice/MemoryGame.swift
@@ -0,0 +1,21 @@
+//
+//  MemoryGame.swift
+//  Swift-practice
+//
+//  Created by Ross on 2/4/22.
+//
+
+import Foundation //basic library for Swift that doesn't include a lot of UI stuff
+
+struct MemoryGame {
+    var cards: Array<Card>
+    
+    func choose(_: Card) {
+        
+    }
+    
+    struct Card { //This struct is called MemoryGame.Card outside of this struct
+        
+    }
+    
+}
